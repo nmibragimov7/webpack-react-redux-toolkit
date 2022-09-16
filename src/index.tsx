49c3b66366eee673f273ed7912c1b06bcd3325fd
nibragimov <nibragimov@1cb.kz>
2022-09-16 23:37:38 +0600

    Initial repo

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import {render} from "react-dom";
 import Counter from "./components/Counter";
 
 render(<div>
-    <span>Hello world!</span>
     <Counter/>
 </div>, document.getElementById('root'));
