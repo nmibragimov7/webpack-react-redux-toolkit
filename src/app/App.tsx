de56f96fd43d4f319168e13d31d9a0012cf6bf0d
Nurlan Ibragimov <nibragimov@1cb.kz>
2022-12-13 13:38:05 +0600

    +fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,21 @@ import { Sidebar } from 'widgets/Sidebar';
 // import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 
-const App = () =>
 // const { theme } = useTheme();
 // useEffect(() => {
 //     if (Math.random() < 0.5) {
 //         throw new Error(); // выкидывает ошибку что проект сломался
 //     }
 // }, []);
-    (
-        <div className={classNames('app', {}, [])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content__page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-        </div>
-    );
+const App = () => (
+    <div className={classNames('app', {}, [])}>
+        <Suspense fallback="">
+            <Navbar />
+            <div className="content__page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
+    </div>
+);
 export default App;
